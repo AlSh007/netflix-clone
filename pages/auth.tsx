bbe4e66fe9116ef42cb4bd233756b8614904c832
AlShPhoenix <alok.11911001@iiitsonepat.ac.in>
2023-04-16 17:11:41 +0530

    added next auth for authentication

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,50 @@
 import Input from "@/components/Input";
+import { useState,useCallback } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import {signIn} from 'next-auth/react'
 
 const Auth =() =>{
+    const router=useRouter();
+    const [email,setEmail]=useState(' ');
+    const [name,setName]=useState(' ');
+    const [password,setPassword]=useState('');
+
+    const [variant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    }, []);
+
+    const login = useCallback(async () => {
+        try {
+          await signIn('credentials', {
+            email,
+            password,
+            redirect: false,
+            callbackUrl: '/'
+          });
+    
+          router.push('/profiles');
+        } catch (error) {
+          console.log(error);
+        }
+      }, [email, password, router]);
+
+    const register = useCallback(async () => {
+        try {
+          await axios.post('/api/register', {
+            email,
+            name,
+            password
+          });
+    
+          login();
+        } catch (error) {
+            console.log(error);
+        }
+      }, [email, name, password, login]);
+    
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -10,21 +54,47 @@ const Auth =() =>{
                 <div className="flex justify-center">
                     <div className="bg-black/70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            Sign in
+                        {variant === 'login' ? 'Sign in' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
+                                {variant === 'register' && (
+                                <Input
+                                id="name"
+                                type="text"
+                                label="Username"
+                                value={name}
+                                onChange={(e: any) => setName(e.target.value)} 
+                                />
+                            )}
                             <Input 
                             label="Email"
-                            onChange={() =>{}}
+                            onChange={(ev:any) =>setEmail(ev.target.value)}
                             type="email"
                             id="email"
-                            value=""
+                            value={email}
+                             />
+                             <Input 
+                            label="Password"
+                            onChange={(ev: any) =>setPassword(ev.target.value)}
+                            type="password"
+                            id="password"
+                            value={password}
                              />
                         </div>
-                    </div>
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            {variant === 'login' ? 'Login' : 'Sign up'}
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                    {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+                    <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                        {variant === 'login' ? 'Create an account' : 'Login'}
+                    </span>
+                    .
+                    </p>
 
                 </div>
             </div>
+        </div>
         </div>
     );
 }
