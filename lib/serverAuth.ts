4e2edc6e875f6808646420955ac6c73e7017f025
AlShPhoenix <alok.11911001@iiitsonepat.ac.in>
2023-04-23 17:07:33 +0530

    setup github oauth and some other changes

---
+++ lib/serverAuth.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+
+import prismadb from '@/lib/prismadb';
+
+const serverAuth = async (req: NextApiRequest, ) => {
+  const session = await getServerSession(req);
+
+  if (!session?.user?.email) {
+    throw new Error('Not signed in');
+  }
+
+  const currentUser = await prismadb.user.findUnique({
+    where: {
+      email: session.user.email,
+    }
+  });
+  
+  if (!currentUser) {
+    throw new Error('Not signed in');
+  }
+
+  return { currentUser };
+}
+
+export default serverAuth;
