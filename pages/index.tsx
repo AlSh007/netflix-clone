d4bf49e3e47a1ca9b5484fbd5410d8d4bcfb8cb9
AlShPhoenix <alok.11911001@iiitsonepat.ac.in>
2023-05-14 16:23:23 +0530

    created Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 import { User } from "@prisma/client";
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
@@ -19,12 +19,9 @@ export async function getServerSideProps(context:NextPageContext) {
 }
 
 export default function Home() {
-  const  { data: user }=useCurrentUser();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Netflix Clone</h1>
-      <p>Logged in as : {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={()=>signOut()}></button>
+      <Navbar/>
     </main>
   )
 }
