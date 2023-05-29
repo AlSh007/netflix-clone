181478b6cf771e42fd51a1606dc6be64bc851559
AlShPhoenix <alok.11911001@iiitsonepat.ac.in>
2023-05-29 22:43:32 +0530

    added code for favorite

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest,NextApiResponse } from "next";
 import { without } from "lodash";
-
+ 
 import prismadb from '@/lib/prismadb';
 import serverAuth from "@/lib/serverAuth";
 
@@ -20,7 +20,48 @@ export default async function handler(req:NextApiRequest, res: NextApiResponse) 
             if(!existingMovie){
                 throw new Error('INVALID ID');
             }
+
+            const user = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                data: {
+                    favoriteIds: {
+                        push: movieId,
+                    }
+                }
+            });
+            return res.status(200).json(user);
         }
+        if(req.method === 'DELETE'){
+            const { currentUser } = await serverAuth(req);
+
+            const { movieId } = req.body;
+
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: movieId,
+                }
+            });
+
+            if(!existingMovie){
+                throw new Error('INVALID ID');
+            }
+
+            const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
+            
+            const updatedUser = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                data: {
+                    favoriteIds: updatedFavoriteIds,
+                }
+            });
+
+            return res.status(200).json(updatedUser);
+        }
+        return res.status(405).end();
     } catch(error){
         console.log(error);
         return res.status(400).end();
